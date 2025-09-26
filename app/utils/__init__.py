7a03fb15f53be6ae640a8734ef92b497aee22058
caua1503 <cauamedinax@gmail.com>
2025-09-26 14:11:43 -0300

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,6 +3,7 @@ from .helpers import (
     extract_host_port_from_request,
     get_method_and_target_from_request,
     parse_headers_from_request,
+    get_content_length_from_request,
     strip_proxy_authorization_header,
 )
 
@@ -10,6 +11,7 @@ __all__ = [
     "extract_host_port_from_request",
     "get_method_and_target_from_request",
     "parse_headers_from_request",
+    "get_content_length_from_request",
     "strip_proxy_authorization_header",
     "ensure_connection_close_header",
 ]
